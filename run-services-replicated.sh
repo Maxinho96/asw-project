4534db4c9b3a0e530d09287e505d08f294a2f61b
Unknown <maxbrux@live.it>
2019-06-19 19:29:48 +0200

    Refinements

--- run-services-replicated.sh
+++ run-services-replicated.sh
@@ -7,11 +7,15 @@ echo Running Application
 echo Starting Eureka Server
 java -Xms64m -Xmx128m -jar eureka-server/build/libs/eureka.jar &
 
-echo Starting A Service *2
+echo Starting A Service *4
+java -Xms64m -Xmx128m -jar a-service/build/libs/aservice.jar &
+java -Xms64m -Xmx128m -jar a-service/build/libs/aservice.jar &
 java -Xms64m -Xmx128m -jar a-service/build/libs/aservice.jar &
 java -Xms64m -Xmx128m -jar a-service/build/libs/aservice.jar &
 
-echo Starting C Service *2
+echo Starting C Service *4
+java -Xms64m -Xmx128m -jar c-service/build/libs/cservice.jar &
+java -Xms64m -Xmx128m -jar c-service/build/libs/cservice.jar &
 java -Xms64m -Xmx128m -jar c-service/build/libs/cservice.jar &
 java -Xms64m -Xmx128m -jar c-service/build/libs/cservice.jar &
 
