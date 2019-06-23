704475df08f78b06d8dc5ffdc5f907ef5e581090
Unknown <maxbrux@live.it>
2019-06-23 19:09:47 +0200

    Updated bash scripts

--- run-curl-client-forever.sh
+++ run-curl-client-forever.sh
@@ -2,7 +2,9 @@
 
 # itera la richiesta all'infinito 
 
-while true; do 
+while true; do
+    curl -X POST localhost:8080/aservice
+    echo ""
 	curl localhost:8080/aservice
 	echo "" ; 
 done 
--- run-curl-client.sh
+++ run-curl-client.sh
@@ -2,7 +2,9 @@
 
 # itera la richiesta 100 volte 
 
-for i in {1..100}; do 
+for i in {1..100}; do
+    curl -X POST localhost:8080/aservice
+    echo ""
 	curl localhost:8080/aservice
 	echo "" ; 
 done 
